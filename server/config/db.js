b51328404705099294ad30ea11e778375b9527af
Kazi Islam <kzislam@gmail.com>
2024-11-07 11:18:00 -0500

    Revert "Added authentication, loginUser, & logoutUser"
    This reverts commit 7268ccd5a2c71ed26a06c9355e2077498ad99c49.

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //# database connection file - db.js
 import mongoose from "mongoose";
 
@@ -21,16 +20,3 @@ const connectDB = async () => {
 };
 
 export { connectDB };
-=======
-import mongoose from "mongoose"
-
-export const connectDB = async() => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.error(`Error: ${error.message}`)
-        process.exit(1) 
-    }
-}
->>>>>>> 5a48d96 (Added authentication, loginUser, & logoutUser)
