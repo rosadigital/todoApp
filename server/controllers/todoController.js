12f651b5b5dca801791d7ac6a7fd974c299ec534
Kazi Islam <kzislam@gmail.com>
2024-11-08 16:25:52 -0500

    To do create and get

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,5 +1,20 @@
-import Todo from '../models/todo.js';
+import Todo from "../models/todo.js";
 
+const createTodo = async (req, res) => {
+  try {
+    console.log("req.user:", req.user);
+    const newTodo = new Todo({
+      userId: req.user.id,
+      title: req.body.title,
+      description: req.body.description,
+      status: req.body.status,
+    });
+    const todo = await newTodo.save();
+    res.status(201).json(todo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const getTodos = async (req, res) => {
   try {
@@ -10,6 +25,4 @@ const getTodos = async (req, res) => {
   }
 };
 
-
-
-export default {getTodos };
+export default { createTodo, getTodos };
--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -6,8 +6,10 @@ const authMiddleware = (req, res, next) => {
   if (!token) return res.status(401).json({ error: "Access denied" });
 
   try {
+    console.log("JWT Secret:", process.env.JWT_SECRET);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
+    console.log("Decoded token:", decoded);
     next();
   } catch (error) {
     res.status(400).json({ error: "Invalid token" });
