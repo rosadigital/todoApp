06e9b2ab736375d73ccfd8de38a67be1d2deafd0
Osman <osmanarri1984@gmail.com>
2024-11-08 18:53:48 -0500

    added logout user route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,5 +4,6 @@ import authController from "../controllers/authController.js";
 
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
+router.post("/logout", authController.logoutUser);
 
 export { router as authRoutes };
