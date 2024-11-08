7222c6cf1838616e08c738e8c0a2154eed005aff
Osman <osmanarri1984@gmail.com>
2024-11-07 21:56:50 -0500

    added user login function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,4 +16,23 @@ const registerUser = async (req, res) => {
   }
 };
 
-export default { registerUser };
+// Osman
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export default { registerUser, loginUser };
